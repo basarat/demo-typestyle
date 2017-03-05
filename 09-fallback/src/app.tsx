dcd9bee78de59ed0838ea1ee60352c78451b840c
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-03-05 20:58:00 +1100

    setup pros 09

--- 09-fallback/src/app.tsx
+++ 09-fallback/src/app.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { style } from 'typestyle';
 
-const className = style({
-  fontSize: '20px',
-  backgroundColor: 'rgba(200, 54, 54, 0.5)',
-});
+const className = style(
+  {
+    fontSize: '20px',
+    backgroundColor: 'rgba(200, 54, 54, 0.5)',
+  }
+);
 
 ReactDOM.render(
   <div className={className}>
