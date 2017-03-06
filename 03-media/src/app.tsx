8f04d76e26f325d2e9355ffb0743edd56cc123cf
basarat <basaratali@gmail.com>
2017-03-06 13:49:42 +1100

    better start

--- 03-media/src/app.tsx
+++ 03-media/src/app.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { style } from 'typestyle';
-
-const className = style(
-  { color: 'red' },
-);
 
 ReactDOM.render(
-  <div className={className}>
+  <div>
     Hello World
   </div>,
   document.getElementById('root')
