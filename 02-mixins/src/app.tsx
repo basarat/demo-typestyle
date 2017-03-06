ced3deeda36a9b60105fd97d3603dcc7e778ed89
basarat <basaratali@gmail.com>
2017-03-06 11:43:54 +1100

    02 start

--- 02-mixins/src/app.tsx
+++ 02-mixins/src/app.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { style } from "typestyle";
-
-const className = style(
-  { fontSize: '30px' },
-  { color: 'red' },
-);
 
 ReactDOM.render(
-  <div className={className}>
-    Hello World Mixins
+  <div>
+    Hello World
   </div>,
   document.getElementById('root')
 );
