0459207c9af8b48e53210d0e60c6010c067e4438
basarat <basaratali@gmail.com>
2017-03-12 09:15:20 +1100

    start

--- 10-static/src/app.tsx
+++ 10-static/src/app.tsx
@@ -1,33 +1,3 @@
 import * as React from 'react';
-import { style, getStyles } from 'typestyle';
+import { style } from 'typestyle';
 
-const className = style({
-  color: 'red',
-  fontSize: '30px',
-});
-
-const App = () => {
-  return (
-    <div className={className}>
-      Hello World
-    </div>
-  );
-}
-
-import * as ReactDOMServer from 'react-dom/server';
-const html = ReactDOMServer.renderToString(<App />);
-const css = getStyles();
-
-export const renderPage = ({ html, css }) => `
-<html>
-  <head>
-    <style>${css}</style>
-  </head>
-  <body>
-    <div>${html}</div>
-  </body>
-</html>
-`;
-const renderedPage = renderPage({ html, css });
-import * as fs from 'fs';
-fs.writeFileSync(__dirname + '/index.html', renderedPage);
